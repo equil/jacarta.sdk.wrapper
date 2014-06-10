eaff3d53897fdb623afef82938501cde782eafcf
Alexey Rogatkin <alexey.rogatkin@gmail.com>
2014-06-10 12:09:02 +0400

    t

--- ARJaCartaWrapper/ARJaCartaWrapper/dummy.c
+++ ARJaCartaWrapper/ARJaCartaWrapper/dummy.c
@@ -7,8 +7,3 @@ int __attribute__((weak)) iR301SetTimeout(void) {
 int __attribute__((weak)) iR301Transmit(void) {
     return 0;
 }
-
-int lmFunctionsLoader(void) {
-    assert(1);
-    return 0;
-}
--- ARJaCartaWrapper/ARJaCartaWrapper/dummy.h
+++ ARJaCartaWrapper/ARJaCartaWrapper/dummy.h
@@ -1,3 +1,2 @@
 int __attribute__((weak)) iR301Transmit(void);
 int __attribute__((weak)) iR301SetTimeout(void);
-int lmFunctionsLoader(void);
